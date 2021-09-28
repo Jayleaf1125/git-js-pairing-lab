013042250e82ca78b92a51d909a6fc607c032043
EC2 Default User <ec2-user@ip-172-31-26-46.us-east-2.compute.internal>
2021-09-28 18:53:18 +0000

    function 3

--- index.js
+++ index.js
@@ -15,4 +15,16 @@ function multiplesOfThree() {
 
 // Call
 // fiveToOneHundred();
-multiplesOfThree();
+//multiplesOfThree();
+
+
+function multiplesOfThreeOrFive() {
+    for(i = 0; i <= 100; i++) {
+        if(i % 3 === 0) {
+         console.log(i);
+        }  else if (i % 5 === 0) {
+            console.log(i);
+        }
+    }
+}
+    multiplesOfThreeOrFive();
