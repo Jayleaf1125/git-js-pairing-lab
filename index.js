daba00923df85eb1e4122eec19040331b70f6e92
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-09-28 18:38:11 +0000

    function 2 completed

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 //Code your solutions in this file
 function fiveToOneHundred() {
-    for(let i = 5; i <= 100; i++ ) {
-    
-    console.log(i);    
+    for(let i = 5; i <= 100; i++) {
+        console.log(i);    
     }
-     
 }
-fiveToOneHundred();
+
+function multiplesOfThree() {
+    for(let i = 1; i <= 100; i++) {
+        if (i % 3 === 0) {
+            console.log(i);
+        }
+    }
+}
+
+// Call
+// fiveToOneHundred();
+multiplesOfThree();
