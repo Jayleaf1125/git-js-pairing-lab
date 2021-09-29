22165c6681b5572bd22c18350332d16e656cc25e
Jayleaf1125 <Jasonpadilla104@gmail.com>
2021-09-29 19:20:18 +0000

    All non-bonus functions completed

--- index.js
+++ index.js
@@ -75,10 +75,26 @@ function add(number1, number2) {
     }
 }
 
+function isNegative(number) {
+    if (number < 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 function traingleArea(base, height) {
     const product = base * height;
     const area = product * (1 / 2);
     return area;
+}
+
+function betweenTwentyAndFourty(number) {
+    if (number < 100 && number > 20) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function largest(number1, number2, number3) {
@@ -87,11 +103,13 @@ function largest(number1, number2, number3) {
 }
 
 // Call
-// fiveToOneHundred();
-// multiplesOfThree();    
-// multiplesOfThreeOrFive();
-// untilNum(5);
-// console.log(add(6, 6));
-// console.log(multiply(3, 7.5));
-// console.log(traingleArea(6, 8));
+fiveToOneHundred();
+multiplesOfThree();    
+multiplesOfThreeOrFive();
+untilNum(5);
+console.log(add(6, 6));
+console.log(multiply(3, 7.5));
+console.log(isNegative(Math.PI));
+console.log(traingleArea(6, 8));
+console.log(betweenTwentyAndFourty(101));
 console.log(largest(30, 22, 17));
